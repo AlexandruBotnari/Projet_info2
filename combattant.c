2b6d1ad68d72bca6d7c11d0db1cba6ac39db40a0
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-28 15:46:11 +0200

    Update combattant.c

--- combattant.c
+++ combattant.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include "combattant.h"
+
+void afficher_combattant(Combattant c) {
+    printf("Nom: %s\n", c.nom);
+    printf("Vie: %d/%d\n", c.vie_courante, c.vie_max);
+    printf("Attaque: %d\n", c.attaque);
+    printf("Defense: %d\n", c.defense);
+    printf("Agilite: %d\n", c.agilite);
+    printf("Vitesse: %d\n", c.vitesse);
+}
 

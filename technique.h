6d1ed52291abcafff3682d78519b08f0ee529bcf
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-28 15:43:13 +0200

    Update technique.h

--- technique.h
+++ technique.h
@@ -1,1 +1,15 @@
+#ifndef TECHNIQUE_H
+#define TECHNIQUE_H
+
+typedef struct {
+    char nom[50];
+    char description[200];
+    int valeur;
+    int type_effet;
+    int tours_actifs;
+    int recharge;
+    int recharge_restante;
+} TechniqueSpeciale;
+
+#endif
 

3a3ee6e369cf90fd278c4e8295a1bb567c75068b
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-28 15:48:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "combattant.h"
+#include "combattant.c" // juste pour tester pour l'instant
+
+int main() {
+    Combattant monCombattant = {"DragonRouge", 100, 100, 30, 20, 15, 10, {}, 0};
+    afficher_combattant(monCombattant);
+    
+    return 0;
+}

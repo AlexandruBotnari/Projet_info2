eed0f4c2c82050172e05c0e8c34d8ed0141d3c03
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-29 18:06:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include "combattant.h"
 #include "combattant.c" // juste pour tester pour l'instant
-
-int main() {
-    Combattant monCombattant = {"DragonRouge", 100, 100, 30, 20, 15, 10, {}, 0};
-    afficher_combattant(monCombattant);
-    
-    return 0;
+int choix_personnage(){
+	int p;
+	printf("Quel personnage voulez vous:\nZeus: 0\narthemis: 1\n");
+	scanf("%d", &p);
+	return p;
 }
+int main() {
+	char *tab_personnage[]={"Zeus.txt","arthemis.txt"};
+	int index= choix_personnage();
+	if (index < 0 || index >= 2) {
+        printf("Choix invalide.\n");
+        return 1;
+        }
+	Combattant monCombattant = charger_combattant(tab_personnage[index]);
+	afficher_combattant(monCombattant);
+	return 0;
+}
+	

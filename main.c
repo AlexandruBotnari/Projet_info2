e446c3256fbeedd7157f37b752b193ed89bffaf3
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-29 18:27:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 #include "combattant.c" // juste pour tester pour l'instant
 int choix_personnage(){
 	int p;
-	printf("Quel personnage voulez vous:\nZeus: 0\narthemis: 1\n");
+	printf("Quel personnage voulez vous:\nArtémis : 0\nAtlas: 1\nDéméter : 2\nGaia : 3\nHélios : 4\nHéphaistos : 5\nOuranos : 6\nPoséidon : 7\nTéthys : 8\nZeus : 9\n ");
 	scanf("%d", &p);
 	return p;
 }
 int main() {
-	char *tab_personnage[]={"Zeus.txt","arthemis.txt"};
+	char *tab_personnage[]={"Arthemis.txt","Atlas.txt","déméter.txt","gaia.txt","hélios.txt","Héphaistos.txt","Ouranos.txt","poséidon.txt","téthys.txt","zeus.txt"};
 	int index= choix_personnage();
-	if (index < 0 || index >= 2) {
+	if (index < 0 || index >= 9) {
         printf("Choix invalide.\n");
         return 1;
         }

121e03604ae10840a5a20748267def13fa16201f
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-30 16:12:28 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,1 +1,22 @@
+#ifndef COMBATTANT_H
+#define COMBATTANT_H
+#include "technique.h"
+
+typedef struct {
+    char nom[50];
+    int vie_max;
+    int vie_courante;
+    int attaque;
+    int defense;
+    int agilite;
+    int vitesse;
+    TechniqueSpeciale techniques[5];
+    int nb_techniques;
+    char element[50];
+    int charge;
+}Combattant;
+int choix_personnage();
+void afficher_combattant(Combattant c);
+Combattant charger_combattant(char *chemin_fichier);
+#endif
 

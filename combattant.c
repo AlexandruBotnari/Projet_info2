b851a1267dfbdcac4dc9b3ae284b101a73877b1f
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-30 11:12:42 +0200

    Update combattant.c

--- combattant.c
+++ combattant.c
@@ -35,18 +35,6 @@ Combattant charger_combattant(char *chemin_fichier){
 	
 	fclose(f);
 	return c;
-}#include <stdio.h>
-#include "combattant.h"
-#include <string.h>
-void afficher_combattant(Combattant c) {
-    printf("Nom: %s\n", c.nom);
-    printf("Vie: %d/%d\n", c.vie_courante, c.vie_max);
-    printf("Attaque: %d\n", c.attaque);
-    printf("Defense: %d\n", c.defense);
-    printf("Agilite: %d\n", c.agilite);
-    printf("Vitesse: %d\n", c.vitesse);
-    printf("Element: %s\n", c.element);
-    printf("Technique spéciale: %s\n", c.technique_spéciale);
 }
 
 Combattant charger_combattant(char *chemin_fichier){

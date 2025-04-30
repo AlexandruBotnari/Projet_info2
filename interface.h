5e8fce61c616a2924f6eb23c194648db294c47a7
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-30 15:23:05 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,1 +1,10 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "combat.h"   // contient la struct Combat, Equipe, Combattant, Technique
+
+// Affiche intégralement l’état du combat dans la console
+void afficher_interface(const Combat *combat);
+
+#endif // INTERFACE_H
 

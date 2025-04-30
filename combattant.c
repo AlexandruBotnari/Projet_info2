dfe3f4eb52a2e7d4df618ca75fe690a6f946b5cb
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-04-30 10:46:54 +0200

    Update combattant.c

--- combattant.c
+++ combattant.c
@@ -9,6 +9,44 @@ void afficher_combattant(Combattant c) {
     printf("Agilite: %d\n", c.agilite);
     printf("Vitesse: %d\n", c.vitesse);
     printf("Element: %s\n", c.element);
+    printf("Technique spéciale: %s\n", c.technique_spéciale);
+}
+
+Combattant charger_combattant(char *chemin_fichier){
+	Combattant c;
+	FILE* f;
+	f=fopen(chemin_fichier,"r");
+	if (f == NULL){
+		printf("erreur fopen\n");
+		strcpy(c.nom, "inconnu");
+		c.vie_max = c.vie_courante = c.attaque = c.defense = c.agilite = c.vitesse = 0;
+		strcpy(c.element, "aucun");
+		return c;
+	}
+	fscanf(f, "%s %d %d %d %d %d %d %s",
+           c.nom,
+           &c.vie_max,
+           &c.vie_courante,
+           &c.attaque,
+           &c.defense,
+           &c.agilite,
+           &c.vitesse,
+           c.element);
+	
+	fclose(f);
+	return c;
+}#include <stdio.h>
+#include "combattant.h"
+#include <string.h>
+void afficher_combattant(Combattant c) {
+    printf("Nom: %s\n", c.nom);
+    printf("Vie: %d/%d\n", c.vie_courante, c.vie_max);
+    printf("Attaque: %d\n", c.attaque);
+    printf("Defense: %d\n", c.defense);
+    printf("Agilite: %d\n", c.agilite);
+    printf("Vitesse: %d\n", c.vitesse);
+    printf("Element: %s\n", c.element);
+    printf("Technique spéciale: %s\n", c.technique_spéciale);
 }
 
 Combattant charger_combattant(char *chemin_fichier){

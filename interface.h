94b2b61c0fe9a379fc227c3969208e36240df8d3
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-04 18:49:35 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -5,6 +5,13 @@
 
 // Affiche intégralement l’état du combat dans la console
 void afficher_interface(const Combat *combat);
+void tracer_bordure(const char *gauche, const char *sep, const char *droite);
+void afficher_entete(const char *nom);
+void afficher_barre(int actuel, int maxi);
+void afficher_prochain(int idxEq, int idxCbt, const Combat *c);
+void afficher_effets(const Combattant *cbt);
+void afficher_ligne_combattant(const Combattant *cbt, int idxEq, int idxCbt, const Combat *c);
+void afficher_separateur(void);
 
 #endif // INTERFACE_H
 

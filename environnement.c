63d1052628d3391598d3ef1300cfdd1a36eba0a8
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-05 15:28:24 +0200

    Update environnement.c

--- environnement.c
+++ environnement.c
@@ -2,4 +2,24 @@
 #include <string.h>
 #include <stdio.h>
 
-void charger_environnement(Environnement e)
+Environnement charger_environnement(char *chemin_fichier){
+	Environnement e;
+	FILE* f;
+	f=fopen(chemin_fichier,"r");
+	if (f == NULL){
+		printf("erreur fopen\n");
+		strcpy(c.nom, "inconnu");
+		e.nom = e.element_avantage = e.element_desavantage = e.bonus = e.malus = 0;
+		strcpy(c.element, "aucun");
+		return c;
+	}
+	fscanf(f, "%s %s %s %f %f",
+           e.nom,
+           e.element_avantage,
+           e.element_desavantage,
+           &e.bonus,
+           &e.malus);
+    
+	fclose(f);
+	return e;
+}

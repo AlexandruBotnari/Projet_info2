147a90c8f9bac117aaa5147ef149aae5d5483a63
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-06 16:07:57 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -11,5 +11,6 @@ void jouer_tour(Equipe *e1, Equipe *e2);
 int choix_cible_a_attaquer(Equipe defenseur);
 void fin_tour(Combattant *perso);
 int calculer_degats_generique(int valeur_base,int valeur_defense,const char *type_attaquant,const char *type_defenseur, Environnement env);
+void fin_tour(Combattant *perso);
 
 #endif

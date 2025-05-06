ff882585ca6a91036dc50078e8aef4dcc5eec0cb
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-06 16:05:21 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,3 +1,4 @@
+#ifnded COMBAT_H
 #define COMBAT_H
 #include "equipe.h" 
 typedef struct {
@@ -9,3 +10,6 @@ void jouer_combat(Equipe *e1, Equipe *e2);
 void jouer_tour(Equipe *e1, Equipe *e2);
 int choix_cible_a_attaquer(Equipe defenseur);
 void fin_tour(Combattant *perso);
+int calculer_degats_generique(int valeur_base,int valeur_defense,const char *type_attaquant,const char *type_defenseur, Environnement env);
+
+#endif

6392ab95ccfd560dc15dda677da1c2b9a790803f
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-06 16:14:41 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,17 +1,27 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#include "combat.h"   // contient la struct Combat, Equipe, Combattant, Technique
+#include <locale.h>            
+#include "combat.h"            
+#include "equipe.h"
+#include "combattant.h"        
+#include "technique.h"         
+#include "couleurs_console.h"
 
 // Affiche intégralement l’état du combat dans la console
 void afficher_interface(const Combat *combat);
+
+// Tracé des bordures en box-drawing
 void tracer_bordure(const char *gauche, const char *sep, const char *droite);
+
+// Divers éléments d’affichage
 void afficher_entete(const char *nom);
 void afficher_barre(int actuel, int maxi);
 void afficher_prochain(int idxEq, int idxCbt, const Combat *c);
 void afficher_effets(const Combattant *cbt);
-void afficher_ligne_combattant(const Combattant *cbt, int idxEq, int idxCbt, const Combat *c);
+void afficher_ligne_combattant(const Combattant *cbt,
+                               int idxEq, int idxCbt,
+                               const Combat *c);
 void afficher_separateur(void);
 
 #endif // INTERFACE_H
-

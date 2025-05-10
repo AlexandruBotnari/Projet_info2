429dbfd1e90188983e0e1ad31464ee70848326f4
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-10 16:58:37 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -4,7 +4,6 @@
 #include "combattant.h"
 #include "equipe.h"
 #include "environnement.h"
-#include "menu.h"
 
 typedef struct {
     Combattant *combattant;
@@ -12,10 +11,13 @@ typedef struct {
     Equipe *equipe_def;
 } OrdreCombattant;
 
+int choix_cible_a_attaquer(Equipe *defenseur);
+int calculer_degats_generique(int valeur_base, int valeur_defense, const char *element_attaquant, const char *element_defenseur, Environnement *env);
 void jouer_tour(Equipe *e1, Equipe *e2, Environnement *env);
 void jouer_combat(Equipe *e1, Equipe *e2, Environnement *env);
-int choix_cible_a_attaquer(Equipe *defenseur);
-int calculer_degats_generique(int valeur_base, int valeur_defense,const char *element_attaquant, const char *element_defenseur, Environnement *env);
+void fin_tour(Equipe *equipe);
+void utiliser_technique_speciale(Combattant *utilisateur, Equipe *equipe_joueur, Equipe *equipe_adverse, Environnement *env);
 
 #endif
+
 

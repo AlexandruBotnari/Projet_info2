32de5231fbc762c0fa862b7e3395daca1e9e564e
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-10 16:27:07 +0200

    Update equipe.h

--- equipe.h
+++ equipe.h
@@ -1,12 +1,18 @@
-
+#ifndef EQUIPE_H
 #define EQUIPE_H
+
 #include "combattant.h"
-typedef struct{
-	char nom[50];
-	Combattant combattants[3];
-	int nb_combattants;
-}Equipe;
-Equipe creer_equipe();
-void afficher_equipe(Equipe e);
-int equipe_est_vaincue(Equipe e);
+
+typedef struct {
+    char nom[50];
+    Combattant combattants[3];
+    int nb_combattants;
+} Equipe;
+
+Equipe creer_equipe(void);
+void afficher_equipe(Equipe *e);
+void afficher_equipe_tour(Equipe *e);
+int equipe_est_vaincue(Equipe *e);
+
+#endif
 

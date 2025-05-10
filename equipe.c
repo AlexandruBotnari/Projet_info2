78485c43c5d01ec141502e3523c2dea2f4f3bcae
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-10 16:32:59 +0200

    Update equipe.c

--- equipe.c
+++ equipe.c
@@ -1,41 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "equipe.h"
-#include "combattant.h"
+#include "menu.h"          
+#include "combattant.h" 
 
-Equipe creer_equipe(){
-	Equipe e;
-	int choix_personnage();
-	
-    	char *tab_personnage[] = {"Arthemis.txt","Atlas.txt","déméter.txt","gaia.txt","hélios.txt","Héphaistos.txt","Ouranos.txt","poséidon.txt","téthys.txt","zeus.txt"};
-	printf("Entrez le nom de votre équipe : ");
-    	scanf("%s", e.nom);
-    	printf("Création de l'équipe %s :\n", e.nom);
-    	e.nb_combattants = 0; 
-    	while(e.nb_combattants<3){
-    		int index= choix_personnage();
-		if (index < 0 || index > 9) {
-        		printf("Choix invalide.\n");
-        	continue;
-        	}
-		e.combattants[e.nb_combattants] = charger_combattant(tab_personnage[index]);
-    		e.nb_combattants ++;
-	}
-    	return e;
+extern int choix_personnage(); // fonction définie ailleurs
+extern Combattant charger_combattant(const char *chemin_fichier);
+
+Equipe creer_equipe() {
+    Equipe e;
+    char *tab_personnage[] = {
+        "Artémis.txt", "Atlas.txt", "déméter.txt", "gaia.txt", "hélios.txt",
+        "Héphaistos.txt", "Ouranos.txt", "poséidon.txt", "téthys.txt", "zeus.txt"
+    };
+
+    printf("Entrez le nom de votre équipe : ");
+    fgets(e.nom, sizeof(e.nom), stdin);
+    e.nom[strcspn(e.nom, "\n")] = 0;
+
+    e.nb_combattants = 0;
+    while (e.nb_combattants < 3) {
+        int index = choix_personnage();
+        e.combattants[e.nb_combattants] = charger_combattant(tab_personnage[index]);
+        e.nb_combattants++;
+    }
+    return e;
 }
 
-void afficher_equipe(Equipe e) {
-    printf("Équipe : %s\n", e.nom);
-    for (int i = 0; i < e.nb_combattants; i++) {
-        printf("Combattant %d :\n", i + 1);
-        afficher_combattant(e.combattants[i]);
+void afficher_equipe(Equipe *e) {
+    clearTerminal();
+    printf("Équipe : %s\n", e->nom);
+    for (int i = 0; i < e->nb_combattants; i++) {
+        printf("Combattant %d : %s\n", i + 1, e->combattants[i].nom);
     }
 }
-int equipe_est_vaincue(Equipe e) {
-    for (int i = 0; i < e.nb_combattants; i++) {
-        if (e.combattants[i].vie_courante > 0) {
-            return 0; 
+
+void afficher_equipe_tour(Equipe *e) {
+    printf("Équipe : %s\n", e->nom);
+    for (int i = 0; i < e->nb_combattants; i++) {
+        printf("Combattant %d : %s\n", i + 1, e->combattants[i].nom);
+        printf("Vie: %d/%d\n", e->combattants[i].vie_courante, e->combattants[i].vie_max);
+        printf("Charge: %d\n", e->combattants[i].charge);
+    }
+}
+
+int equipe_est_vaincue(Equipe *e) {
+    for (int i = 0; i < e->nb_combattants; i++) {
+        if (e->combattants[i].vie_courante > 0) {
+            return 0;
         }
     }
-    return 1; 
+    return 1;
 }
+
+

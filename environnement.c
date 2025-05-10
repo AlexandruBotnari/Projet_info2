42981e69091d4f0e3ee7c1fff2d7b958710baf87
AlexandruBotnari <101938710+AlexandruBotnari@users.noreply.github.com>
2025-05-10 16:46:04 +0200

    Update environnement.c

--- environnement.c
+++ environnement.c
@@ -1,6 +1,7 @@
-#include "environnement.h"
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "environnement.h"
 
 Environnement charger_environnement(const char *chemin_fichier) {
     Environnement e;
@@ -9,8 +10,8 @@ Environnement charger_environnement(const char *chemin_fichier) {
     if (f == NULL) {
         printf("Erreur lors de l'ouverture du fichier : %s\n", chemin_fichier);
         strcpy(e.nom, "inconnu");
-        strcpy(e.element_avantage, "aucun");
-        strcpy(e.element_desavantage, "aucun");
+        strcpy(e.element_bonus, "aucun");
+        strcpy(e.element_malus, "aucun");
         e.bonus = 0.0f;
         e.malus = 0.0f;
         return e;
@@ -18,11 +19,27 @@ Environnement charger_environnement(const char *chemin_fichier) {
 
     fscanf(f, "%s %s %s %f %f",
            e.nom,
-           e.element_avantage,
-           e.element_desavantage,
+           e.element_bonus,
+           e.element_malus,
            &e.bonus,
            &e.malus);
 
     fclose(f);
     return e;
+}
+
+Environnement choix_environnement() {
+    int p;
+    Environnement env;
+    do {
+        printf("Quel environnement voulez-vous ?\n"
+               "0: Etna 🔥\n1: Nysa 🌲\n2: Keraunos ⚡\n3: Thalassa 🌊\n4: Olympe 🏔️\n"
+               "5: Aulis 🌪️\n");
+        scanf("%d", &p);
+        while(getchar() != '\n'); // vide le buffer
+    } while (p < 0 || p > 5);
+    char *tab_environnement[] = {"etna.txt", "nysa.txt", "keraunos.txt", "thalassa.txt", "olympe.txt",
+        "aulis.txt"};
+    env = charger_environnement(tab_environnement[p]);
+    return env;
 }
